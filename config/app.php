0b310e9a1645b445b43daf075bb2cbb62b30435c
quangnn73 <quangnn73@ghtk.co>
2023-05-19 17:31:05 +0700

    feat(feature/phase-1): add service member, image, event

--- config/app.php
+++ config/app.php
@@ -168,6 +168,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Services\Member\Providers\MemberServiceProvider::class,
+        App\Services\Event\Providers\EventServiceProvider::class,
+        App\Services\Image\Providers\ImageServiceProvider::class
     ])->toArray(),
 
     /*
